1945b61c831220064992ee88398ab489bd23569b
Matheus Entringer <matheusentringer1@gmail.com>
2022-11-01 15:32:38 -0300

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Search, ShoppingCartOutlined } from '@mui/icons-material'
+import { Badge } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -8,6 +10,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `
 
@@ -16,16 +19,44 @@ const Language = styled.span`
   cursor: pointer;
 `
 
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`
+
+const Input = styled.input`
+  border: none;
+`
+
+const Logo = styled.h1`
+  font-weight: bold;
+`
+
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `
 
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `
 
 const Navbar = () => {
@@ -34,9 +65,21 @@ const Navbar = () => {
       <Wrapper>
         <Left>
           <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <Search style={{color:"gray", fontSize: 16}} />
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center><Logo>LAMA.</Logo></Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   )
