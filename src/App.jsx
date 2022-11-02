ae2ae5cd43894fec472f1ea895a1670f78b7c61a
Matheus Entringer <matheusentringer1@gmail.com>
2022-11-02 14:46:19 -0300

    Single Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import Home from "./pages/Home"
-import ProductList from "./pages/ProductList"
+// import ProductList from "./pages/ProductList"
+import Product from "./pages/Product"
 
 const App = () => {
   // return <Home />
-  return <ProductList />
+  // return <ProductList />
+  return <Product />
 }
 
 export default App
