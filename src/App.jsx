da80f54384017d008ef6d29e4950066b67549839
Matheus Entringer <matheusentringer1@gmail.com>
2022-11-02 15:20:01 -0300

    Login and Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // import Home from "./pages/Home"
 // import ProductList from "./pages/ProductList"
-import Product from "./pages/Product"
+// import Product from "./pages/Product"
+// import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 const App = () => {
   // return <Home />
   // return <ProductList />
-  return <Product />
+  // return <Product />
+  // return <Register />
+  return <Login />
 }
 
 export default App
